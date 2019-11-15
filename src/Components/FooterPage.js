3d239cb97a044f729bd636562e446edd62f2e7b0
CreckDev <tiago.kise@hotmail.com>
2019-11-15 14:23:07 -0300

    footer responsivo

--- src/Components/FooterPage.js
+++ src/Components/FooterPage.js
@@ -5,34 +5,27 @@ import InstaLogo from '../img/logoinstawhite.png';
 
 
 
-function FooterPage() {
-    return (
-      <>        
-        <div className="footerDiv">
-          <p className="footerP1">Rasil Borrachas. Atuando há mais de 30 anos no mercado de artefatos de borracha.
-          </p>
-          <p className="footerP2">Contato:  55 (0**11) 4712-4597 / 4712-9104
-          </p>
-          <p className="footerP2">Endereço: Rua Quintino Bocaiúva, 1283 - São Roque – São Paulo – Brasil
-          </p>
-         
-          <ul className="LinksFooter">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Qualidade</a></li>
-            <li><a href="#">Produtos</a></li>
-            <li><a href="#">Representantes</a></li>
-            <li><a href="#">Contato</a></li>
-          </ul>
-          <div className="iconesRedesSociais">
-            <a target="_blank" href="#"><img className="facelogo" src={FacebookLogo} alt='Facebook'/></a>
-            <a target="_blank" href="#"><img className="instalogo" src={InstaLogo} alt='Instagram'/></a>
-          </div>
+export default function FooterPage() {
+  return (
 
+    <div className="footerPage">
+      <p className="footerAddress">Rasil Borrachas. Atuando há mais de 30 anos no mercado de artefatos de borracha.
+      <br></br>Contato:  55 (0**11) 4712-4597 / 4712-9104
+      <br></br>Endereço: Rua Quintino Bocaiúva, 1283 - São Roque – São Paulo – Brasil</p>
+      <div className="divRight">
+        <ul className="linksFooter">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Qualidade</a></li>
+          <li><a href="#">Produtos</a></li>
+          <li><a href="#">Representantes</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+        <div className="iconesRedesSociais">
+          <a target="_blank" href="#"><img className="faceLogo" src={FacebookLogo} alt='Facebook' /></a>
+          <a target="_blank" href="#"><img className="instaLogo" src={InstaLogo} alt='Instagram' /></a>
         </div>
-                     
-       </>          
+      </div>
+    </div>
 
-    );
+  );
 }
-    
-export default FooterPage;
